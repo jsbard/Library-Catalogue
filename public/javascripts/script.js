e40c2e1bf5d3962f936f603ba46a0c424e397c93
Andrew Hickman <hello@andrewhickman.me>
2021-08-01 15:45:30 -0400

    Fix console error when table is not on current page

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,6 +1,15 @@
 "use strict";
 
-paginator({
-    table: document.getElementsByTagName("table")[0],
-    box: document.getElementById("page-buttons")
-});
+const paginateTable = () => {
+    const table = document.getElementsByTagName("table")[0];
+    const box = document.getElementById("page-buttons");
+
+    if (table) {
+        paginator({
+            table: document.getElementsByTagName("table")[0],
+            box: document.getElementById("page-buttons")
+        });
+    }
+}
+
+paginateTable();
